489633fee680bf09786272190566eae4ac90357d
Andreas Albert <andreas.albert@cern.ch>
2020-02-12 21:42:20 -0500

    Nano v6

--- bucoffea/data/datasets/expand_datasets.sh
+++ bucoffea/data/datasets/expand_datasets.sh
@@ -18,14 +18,14 @@ expand(){
 }
 
 INFILE="dataset_names_mc.txt"
-expand "${INFILE}" "RunIISummer16*1June*" | tee datasets_2016.txt
-expand "${INFILE}" "RunIIFall17*1June*" | tee datasets_2017.txt
-expand "${INFILE}" "RunIIAutumn18*1June*" | tee datasets_2018.txt
+expand "${INFILE}" "RunIISummer16*25Oct2019*" | tee datasets_2016.txt
+expand "${INFILE}" "RunIIFall17*25Oct2019*" | tee datasets_2017.txt
+expand "${INFILE}" "RunIIAutumn18*25Oct2019*" | tee datasets_2018.txt
 
 INFILE="dataset_names_data.txt"
-expand "${INFILE}" "Run2016*1June*" | tee -a datasets_2016.txt
-expand "${INFILE}" "Run2017*1June*" | tee -a datasets_2017.txt
-expand "${INFILE}" "Run2018*1June*" | tee -a datasets_2018.txt
+expand "${INFILE}" "Run2016*25Oct2019*" | tee -a datasets_2016.txt
+expand "${INFILE}" "Run2017*25Oct2019*" | tee -a datasets_2017.txt
+expand "${INFILE}" "Run2018*25Oct2019*" | tee -a datasets_2018.txt
 
 sed -i '/BGen/d' datasets_201*.txt
 sed -i '/DYBBJet/d' datasets_201*.txt
@@ -37,6 +37,6 @@ sed -i '/.*LHEWpT_50-150.*/d' datasets_201*.txt
 sed -i '/.*CP5up.*/d' datasets_201*.txt
 sed -i '/.*CP5down.*/d' datasets_201*.txt
 
-sed '/\(Run2016\|\/G.*Jet\)/!d' -i datasets_2016.txt
+# sed '/\(Run2016\|\/G.*Jet\)/!d' -i datasets_2016.txt
 sed -i '/.*JetHT.*/d' datasets_2016.txt
 sed -i '/.*SingleMuon.*/d' datasets_2016.txt
